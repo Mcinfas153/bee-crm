4d8d56dcc009abb1223fea3675ec432c45c0151e
ihlas575 <ihlas575@gmail.com>
2021-08-04 17:48:01 +0530

    NavBar Updated

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -16,10 +16,7 @@
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
             <!-- Navbar Search -->
-            <li class="nav-item">
-                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i class="fas fa-search"></i>
-                </a>
+            <li class="nav-item mt-1">
                 <div class="navbar-search-block">
                     <form class="form-inline">
                         <div class="input-group input-group-sm">
