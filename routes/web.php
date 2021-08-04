5922dda8105f96e134e52c32a28bda572b24eec9
Mc Infas <mcinfas9394@gmail.com>
2021-08-04 11:04:56 +0400

    assets updated and login,regsiter page updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Login;
+use App\Http\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', Login::class);
+Route::get('/register', Register::class);
