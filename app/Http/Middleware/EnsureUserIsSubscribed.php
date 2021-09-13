ba70ca1bde30b02066bf042ecd46a37a3b339d7c
Mc Infas <mcinfas9394@gmail.com>
2021-09-13 10:34:02 +0400

    subcribtion middleware updated

--- app/Http/Middleware/EnsureUserIsSubscribed.php
+++ app/Http/Middleware/EnsureUserIsSubscribed.php
@@ -17,7 +17,7 @@ class EnsureUserIsSubscribed
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->user() && ! $request->user()->subscribed('silver') && Auth::user()->utype == config('usertypes.admin') && Auth::user()->email != 'mcinfas9394@gmail.com') {
+        if (! Auth::user()->subscribed('default') && Auth::user()->utype == config('usertypes.admin')) {
             // This user is not a paying customer...
             return redirect('plans');
         }
