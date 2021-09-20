f308e841f84d6725f66fcb4fa34dd576db2d1a01
Mc Infas <mcinfas9394@gmail.com>
2021-09-20 15:01:33 +0400

    timezone issue reolve in dashboard daily leads

--- config/application.php
+++ config/application.php
@@ -3,5 +3,8 @@
 return [
     'appVersion' => '1.0 BETA',
     'webUrl' => 'https://beeonline.xyz/',
-    'webAppName' => 'Bee CRM'
+    'webAppName' => 'Bee CRM',
+    'serverTimeZone' => env('SERVER_TIME_ZONE'),
+    'mySQLTimezone' => env('MYSQL_TIME_ZONE'),
+    'localTimezone' => 'Asia/Dubai'
 ];
