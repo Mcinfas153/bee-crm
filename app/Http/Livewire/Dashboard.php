30587f15c85554d5504adace3f6d0960729f78f6
Mc Infas <mcinfas9394@gmail.com>
2021-09-22 11:51:44 +0400

    today leads sql timezone issue change

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -34,6 +34,7 @@ class Dashboard extends Component
                 
         
         $todayLeads = DB::select($todayLeadSql);
+        //$todayLeads = Lead::where('created_by', Auth::user()->id)->whereDate('created_at', Carbon::today())->get();
         $followupLeads = Lead::where([['status', config('leadstatus.following')],['created_by', Auth::user()->id]])->count();
         $notInterestedLeads = Lead::where([['status', config('leadstatus.not_interested')],['created_by', Auth::user()->id]])->count();
         $closeLeads = Lead::where([['status', config('leadstatus.closed')],['created_by', Auth::user()->id]])->count();
