ec1d6e86cbd65c1336d747fe08379249209d1eae
Mc Infas <mcinfas9394@gmail.com>
2022-07-26 13:34:53 +0400

    Revert "forgot password mail function added"
    This reverts commit 818f1a332e20ace593d6923746c04355539288e6.

--- app/Http/Livewire/ForgotPassword.php
+++ app/Http/Livewire/ForgotPassword.php
@@ -2,45 +2,14 @@
 
 namespace App\Http\Livewire;
 
-use App\Mail\ResetPassword;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Mail;
 
 class ForgotPassword extends Component
 {
-
-    public $email;
-
-    protected $rules = [
-        'email' => 'required|email',
-    ];
-
     public function render()
     {
         return view('livewire.forgot-password')->layout('layouts.guest', [
             'title' => 'Forget Password?'
         ]);;
-    }
-
-    public function forgotPassword()
-    {
-        $this->validate();
-
-        if(DB::table('users')->where('email', $this->email)->count() === 1){
-
-           $urlCode = Str::random(40);
-
-           $forgotPasswordUrl = URL::to('/forgot-password').'/'.$urlCode;
-
-           Mail::to($this->email)->send(new ResetPassword($forgotPasswordUrl));
-
-        } else{
-
-            $this->addError('email', config('msg.12'));
-
-        }
     }
 }
--- resources/views/livewire/forgot-password.blade.php
+++ resources/views/livewire/forgot-password.blade.php
@@ -1,52 +1,49 @@
 <div>
     <div class="login-box forgot-password-box">
-        <div class="card">
-            <div class="login-logo mt-3 mb-0">
-                <a href="{{ URL::to('/') }}"><img src="{{ asset('assets/dist/img/logos/logo.png') }}" alt=""></a>
-            </div>
-            <div class="card-body login-card-body">
-                <h4 class="text-center mb-2 text-dark">Account Recovery</h4>
-                <p class="login-box-msg mb-3">To help keep your account safe, Bee CRM wants to make sure that it’s
-                    really you trying to sign in....</p>
-
-                <div class="forgot-password-box-content">
-                    <h6 class="text-dark mb-2">Get a confirmation mail</h6>
-                    <p class="text-dark mb-3">Check your mails and get a confirmation mail.</p>
-                    <form wire:submit.prevent="forgotPassword">
-                        <div wire:loading wire:target="forgotPassword">
-                            <div class="loading" style="display: block">Processing...</div>
-                        </div>
-                        <div class="input-group mb-2">
-                            <input name="email" class="form-control" placeholder="Email Address"
-                                wire:model.lazy="email">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-envelope"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mb-3 ml-2">
-                            @error('email') <span class="error error__msg"><i class="fas fa-exclamation-triangle"></i>
-                                {{ $message }}</span> @enderror
-                        </div>
-                        <div class="row items-center mb-3">
-                            <div class="col-8">
-                                <div>
-                                    <a href="{{ URL::to('/register') }}"
-                                        class="btn btn-block text-xs text-start">Regiter an account</a>
-                                </div>
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-4">
-                                <button type="submit" class="btn btn-primary btn-block">Next</button>
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <!-- /.login-card-body -->
+    <div class="card">
+        <div class="login-logo mt-3 mb-0">
+            <a href="{{ URL::to('/') }}"><img src="{{ asset('assets/dist/img/logos/logo.png') }}" alt=""></a>
         </div>
+        <div class="card-body login-card-body">
+            <h4 class="text-center mb-2 text-dark">Account Recovery</h4>
+            <p class="login-box-msg mb-3">To help keep your account safe, Bee CRM wants to make sure that it’s really you trying to sign in....</p>
+
+            <div class="forgot-password-box-content">
+                <h6 class="text-dark mb-2">Get a confirmation mail</h6>
+                <p class="text-dark mb-3">Check your mails and get a confirmation mail.</p>
+                <form wire:submit.prevent="login">
+                    <div wire:loading wire:target="login">
+                        <div class="loading" style="display: block">Processing...</div>
+                    </div>
+                    <div class="input-group mb-5">
+                        <input type="email" name="email" class="form-control" placeholder="Email Address" wire:model.lazy="email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 ml-2">
+                        @error('email') <span class="error error__msg"><i class="fas fa-exclamation-triangle"></i>
+                            {{ $message }}</span> @enderror
+                    </div>
+                    <div class="row items-center mb-3">
+                        <div class="col-8">
+                            <div>
+                                <a href="{{ URL::to('/register') }}" class="btn btn-block text-xs text-start">Regiter an account</a>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Next</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.login-card-body -->
     </div>
-    <!-- /.login-box -->
+</div>
+<!-- /.login-box -->
 </div>
